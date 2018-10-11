3fe00f5d34648b48b3a21cbd3197ca63016da655
Kevin RRivera <krivera1015@gmail.com>
2018-10-11 17:03:42 -0400

    Seed File Working, Models correctly Associated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kevin = User.create(name: "Kevin", username: "kriv")
+eric = User.create(name: "Eric", username: "eroc")
+
+chick_fil = Restaurant.create(name: "Chic'Fila", address_1: "81 Prospect Ave", address_2: "Apt 5c", city: "Brooklyn", rating: 5, category: "Mexican")
+wendy = Restaurant.create(name: "Wendys", address_1: "855 Louisiana Ave", address_2: "Apt 5B", city: "Brooklyn", rating: 2, category: "Pizza")
+
+kevin.restaurants << chick_fil
